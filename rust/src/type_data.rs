a41478680efdecadb80e890b9b03dbc476936f2f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-15 20:47:50 +0200

    type data

---
+++ rust/src/type_data.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x: i32 = 100; // i32 = 32bits(default) / i64 = 64bits
+                      // i = signed, u= unsigned
+    let x: u32 = 100; // i32 = 32bits no receive negative numbers,
+
+    let f = 6.4; // f64 = 64 bits
+
+    print!("{}", f)
+}
