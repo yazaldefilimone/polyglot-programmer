e6ba60eab0a963c418e87bce193db689eda1b844
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-22 22:00:34 +0200

    *

---
+++ c++/beginner/variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+  int first = 10;
+  int second = 20;
+  int three = second + first;
+  std::cout << 'second + first' << second + first << '\n';
+  std::cout << 'second / first' << second / first << '\n';
+  std::cout << 'second * first' << second * first << '\n';
+  std::cout << 'second - first' << second - first << '\n';
+  std::cout << 'three' << three;
+}
