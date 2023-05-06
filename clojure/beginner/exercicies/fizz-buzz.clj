c1724e78fa327f05d6184868a9eaf80b877f880c
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-06 23:27:02 +0200

    *

---
+++ clojure/beginner/exercicies/fizz-buzz.clj
@@ -0,0 +1,10 @@
+(defn divisivel-por? [dividendo divisor]
+  (zero? (mod dividendo divisor)))
+
+
+(defn fizzBuzz [numero]
+  (cond (and (divisivel-por? numero 3)
+             (divisivel-por? numero 5)) "fizzbuzz"
+        (divisivel-por? numero 3) "fizz"
+        (divisivel-por? numero 5) "buzz"
+        :else numero))
