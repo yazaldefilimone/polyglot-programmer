bcec0507a6c4757903af75e163502820f914829f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-07 15:45:22 +0200

    *

--- clojure/beginner/exercicies/fizz-buzz.clj
+++ clojure/beginner/exercicies/fizz-buzz.clj
@@ -4,7 +4,7 @@
 
 (defn fizzBuzz [numero]
   (cond (and (divisivel-por? numero 3)
-             (divisivel-por? numero 5)) "fizzbuzz"
+        (divisivel-por? numero 5)) "fizzbuzz"
         (divisivel-por? numero 3) "fizz"
         (divisivel-por? numero 5) "buzz"
         :else numero))
