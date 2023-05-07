53377ab40cbeaaa63bbde1b570399381978ded0a
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-07 15:45:12 +0200

    *

---
+++ clojure/beginner/variables.clj
@@ -0,0 +1,8 @@
+(def listOfNumber  [1,2,3,4])
+(def listOfStrings ["Y", "A", "Z", "A", "L" , "D", "E"])
+(def me "Yazalde Filimone")
+
+(println (count listOfNumber)) ; 4
+(println (count listOfStrings)) ; 7
+(println me) ; 7
+
