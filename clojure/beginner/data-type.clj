757d8416c891efb4d7c44cdd4b3ca71ce62a9851
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-07 15:58:34 +0200

    *

---
+++ clojure/beginner/data-type.clj
@@ -0,0 +1,13 @@
+(def age 18)
+(def firstName "Yazalde")
+(def height  1.70)
+
+(println (str "Age: " age ", Fist Name: " firstName ", and Height: " height))
+
+(def rationalNumber 1/3)
+(def is true)
+(def isNot false)
+
+(println rationalNumber)
+(println is)
+(println isNot)
