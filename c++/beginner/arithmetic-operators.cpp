94debe97142bf2cb88bd600162b0bbd92f7e4b73
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-08 14:56:48 +0200

    *

---
+++ c++/beginner/arithmetic-operators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+  int a = 10;
+  int b = 20;
+
+  // addition
+  int sum = a + b;
+
+  std::cout << "Sum result: " << sum << std::endl;
+
+  // subtraction
+
+  int difrenrence = a - b;
+
+  std::cout << " Diference : " << difrenrence << std::endl;
+
+  // Multiplication
+
+  int product = a * b;
+  std::cout << " product : " << product << std::endl;
+
+  int quotient = a / b;
+  std::cout << " quotient : " << quotient << std::endl;
+
+  float quotient_with_float = float(a) / float(b);
+
+  std::cout << " quotient_with_float : " << quotient_with_float << std::endl;
+
+  return 0;
+}
