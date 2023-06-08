71f01e5ed8408aedbbdc15affe54d60cec539cf8
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-08 14:48:53 +0200

    *

---
+++ c++/introduction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  int number;
+
+  std::cout << "Enter an integer: ";
+  std::cin >> number;
+
+  std::cout << "You entered: " << number << std::endl;
+
+  return 0;
+}
