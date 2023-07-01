e986dc70cb7307173efb2b6259561b31d03fddeb
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-01 17:23:09 +0200

    *

---
+++ rust/programming-language/src/statements-and-expressions.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let x: f64 = 1.0;
+    let y: f64 = 20.0;
+    let result: f64 = multiply_both(x, y);
+    println!("{}", result);
+    println!("{}", conditionals(10))
+}
+
+//NB: if a functiona ends with an expression it automantically returns that expression
+
+fn multiply_both(x: f64, y: f64) -> f64 {
+    // return x * y; // expression
+    /*
+       return x * y; = statements
+       x * y = expression
+    */
+
+    x * y // automantically return
+}
+
+fn conditionals(cats: i32) -> &'static str {
+    let message: &str = if cats > 1_00 {
+        "To many cats!"
+    } else if cats > 1 {
+        "Multiple cats!"
+    } else {
+        "Need more cats!"
+    };
+    message
+}
