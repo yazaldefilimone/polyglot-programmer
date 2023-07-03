68262762c8dc5e0600118503a6af510891b6212e
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-03 18:45:09 +0200

    *

---
+++ rust/src/methods.rs
@@ -0,0 +1,17 @@
+enum color {
+    reed,
+    green,
+    yellow,
+    rgb,
+}
+
+impl color {
+    fn new(reed: i64, green: i64, yellow: i64) -> Self {
+        let red = color::reed;
+        return red;
+    }
+}
+
+fn main() {
+    let new_color: color = color::new(250, 0, 0);
+}
