a1ab007ef3e8682db8534b46bb3c9898ade48643
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-08 11:43:51 +0200

    *

--- rust/src/slice.rs
+++ rust/src/slice.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let string: String = "Hello World";
-    let slice: &str = &string[0..4];
-    println!("{}", slice)
+    let string: String = String::from("Hello World");
+    let slice: &str = &string[0..5];
+
+    let years: Vec<i64> = vec![1999, 2004, 2009, 2014];
+
+    let two_first_years: &[i64] = &years[0..2];
+
+    println!(
+        "string_slice: {} and two_first_years: {:?}",
+        slice, two_first_years
+    );
 }
