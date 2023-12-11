0bd0c58f045da7cff462811dbea1783c7cc9c7a7
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-11 02:04:32 +0200

    *

---
+++ c++/data-type.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int number = 10;
+  float decimal = 10.10;
+  char character = 'a';
+  bool boolean = true;
+  double double_number = 3.14159265358979323846;
+  long long_number = 1000000000;
+  long long long_long_number = 1000000000000000000;
+  // short short_number = 10;
+  // unsigned int unsigned_number = 10;
+  // unsigned long long unsigned_long_long_number = 1000000000000000000;
+  // unsigned long unsigned_long_number = 1000000000;
+  // unsigned short unsigned_short_number = 10;
+  // wchar_t wide_character = 'a';
+
+  // Derived Data Types
+  int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  return 0;
+}
