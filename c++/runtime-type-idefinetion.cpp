10855e514eef402191f4e68d1a64f0495413fc25
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-11 02:04:32 +0200

    *

--- c++/runtime-type-idefinetion.cpp
+++ c++/runtime-type-idefinetion.cpp
@@ -8,10 +8,10 @@ class Base {
 class Derived : public Base {};
 
 int main() {
+
   Base *base_ptr = new Derived;
 
   // using typeid to get the type of the object
-
   std::cout << "Type: " << typeid(*base_ptr).name() << '\n';
 
   delete base_ptr;
